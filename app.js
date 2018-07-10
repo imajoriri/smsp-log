83c96a29616ec22316d529f7fd992156a8f49321
imajoriri <30540418+imajoriri@users.noreply.github.com>
2018-07-10 19:22:59 +0900

    add api router

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // router
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
